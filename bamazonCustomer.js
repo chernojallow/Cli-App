fa43f7d317887c38be5145124e75ee168207758a
Zika Chea <jallsadou@gmail.com>
2018-08-25 21:28:48 -0500

    Updating the Database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "7082505zika",
+    password: "",
     database: "bamazon"
 });
 
@@ -19,75 +19,94 @@ connection.connect(function (err) {
 });
 
 var afterConnection = function () {
+
+
     connection.query("SELECT * FROM Products ", function (error, results) {
         if (error) throw error;
         console.log(results);
         //connection.end();
 
-    inquirer.prompt([
-        {   name: "item_id",
-            type: "input",
-            message: "What is the ID of the product you want to buy ?",
-           
-            validate: function(value){
-                if (isNaN(value) === false){
-                    return true;
+        inquirer.prompt([
+            {
+                name: "item_id",
+                type: "input",
+                message: "What is the ID of the product you want to buy ?",
+
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
                 }
-                return false;
-            }
 
-        },
-        {
-            name: "stock_quantity",
-            type: "input",
-            message: "How many UNITS of the product you want to buy",
-            validate: function(value){
-        
-                if (isNaN(value)=== false ) {
-                    return true;
+            },
+            {
+                name: "stock_quantity",
+                type: "input",
+                message: "How many UNITS of the product you want to buy",
+                validate: function (value) {
+
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
                 }
-                return false;
-            }
-        },
-    ]).then(function (answer) {
-  
-        var query = "SELECT product_name, department_name, price, stock_quantity FROM products WHERE ?";
-        connection.query(query, {item_id: answer.item_id}, function (error, results) {
-        
-            for (var i = 0; i < results.length; i++) {
-               
-               // console.log(answer.stock_quantity);
-               // console.log("stock_quantity", results[i].stock_quantity);
+            },
+        ]).then(function (answer) {
 
-                 if(answer.stock_quantity < results[i].stock_quantity){
-                 console.log("product_name: " + results[i].product_name + " || department_name: " + results[i].department_name + " || price: " + results[i].price);
+            var query = "SELECT product_name, department_name, price, stock_quantity FROM products WHERE ?";
+            connection.query(query, { item_id: answer.item_id }, function (error, results) {
 
-              }
- 
-             else {
-               console.log("Insufficient amount");
-            }
-           }
-            //console.log("Your products id are: ");
-           // if (error) throw error;
-           
-        
+                for (var i = 0; i < results.length; i++) {
+
+                    // console.log(answer.stock_quantity);
+                    // console.log("stock_quantity", results[i].stock_quantity);
+
+                    if (answer.stock_quantity <= results[i].stock_quantity) {
+                        console.log("-------------------");
+                        var total = results[i].price * answer.stock_quantity;
+                        console.log("Your total cost for", answer.stock_quantity, results[i].product_name, "is:", total);
+                        // console.log("product_name: " + results[i].product_name + " || department_name: " + results[i].department_name + " || price: " + results[i].price);
+                        // updateDatabase(); 
+                    }
+
+                    else {
+                        console.log("Insufficient amount");
+                    }
+                    if (results[i].stock_quantity - answer.stock_quantity) {
+                        connection.query("UPDATE products SET ? WHERE ?",
+
+                            [
+                                {
+                                    stock_quantity: answer.stock_quantity
+                                },
+                                {
+                                    item_id: answer.item_id
+                                }
+                            ],
+
+
+                            function (error, results) {
+
+
+                                console.log("==========================");
+                                console.log(results.affectedRows);
+
+                            });
+                    }
+                }
+                //console.log("Your products id are: ");
+                // if (error) throw error;
+
+
+
+            });
+
         });
-   
-         // tellID();
-       // }
-       
     });
-});
-  
+
 }
 afterConnection();
 
 
-
-
-
-
-
-//tellID();
 
