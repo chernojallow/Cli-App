244322350f082dd5b7c6253cba4e8e03cc16ba16
Zika Chea <jallsadou@gmail.com>
2018-08-27 22:35:53 -0500

    adding a table

--- bamazon.sql
+++ bamazon.sql
@@ -6,9 +6,9 @@ USE bamazon;
 
 CREATE TABLE products (
 
-item_id INT NOT NULL AUTO_INCREMENT;
+item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(45),
-department_name VARCHAR(50);
+department_name VARCHAR(50),
 price DECIMAL(10,2),
 stock_quantity INT(30) NOT NULL,
 PRIMARY KEY (item_id)
@@ -16,31 +16,31 @@ PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Polo Shirt","Clothing", 40, 5);
+VALUES ("Polo Shirt","Clothing", $40, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Air Jordan", "Shoes", 100, 4);
+VALUES ("Air Jordan", "Shoes", $100, 15);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Dining table","Furniture", 70, 2);
+VALUES ("Dining table","Furniture", $70, 12);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Samsung Tv", "Electronics", 80, 1);
+VALUES ("Samsung Tv", "Electronics", $80, 20);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Iphone 10", "Electronics", 500, 6);
+VALUES ("Iphone 10", "Electronics", $500, 16);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("TV Stand", "Furniture ", 150, 3);
+VALUES ("TV Stand", "Furniture ", $150, 30);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Spare Tire", "Auto", 30, 1);
+VALUES ("Spare Tire", "Auto", $30, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("NyQuil", "Health And Beauty", 5, 4);
+VALUES ("NyQuil", "Health And Beauty", $50, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Dove ", "Health And Beauty", 10, 5);
+VALUES ("Dove ", "Health And Beauty", $10, 15);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Milk", "Food", 15, 5);
+VALUES ("Milk", "Food", %15, 50);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
+
+
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+var Table = new require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,21 +12,31 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    // console.log("connected as id" + connection.threadId);
-    //connection.end();
-    console.log("Connected");
-    //afterConnection();
+// connection.connect(function (err) {
+// if (err) throw err;
+// console.log("connected as id" + connection.threadId);
+//connection.end();
+//  console.log("Connected");
+//afterConnection();
 
-});
+// });
 
-var afterConnection = function () {
-
+var showProducts = function () {
 
     connection.query("SELECT * FROM Products ", function (error, results) {
         if (error) throw error;
-        console.log(results);
+        // instantiate table
+        var table = new Table({
+            head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']
+
+        });
+        console.log(" THE TABLE BELOW SHOWS ALL AVAILABLE ITEMS FOR SALE:");
+
+        console.log("----------------------------------------------------")
+        for (var i = 0; i < results.length; i++) {
+            table.push([results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
+        }
+        console.log(table.toString());
         //connection.end();
 
         inquirer.prompt([
@@ -63,7 +76,7 @@ var afterConnection = function () {
                     // console.log("stock_quantity", results[i].stock_quantity);
 
                     if (answer.stock_quantity <= results[i].stock_quantity) {
-                        console.log("-------------------");
+                        console.log("----------------------------------------");
                         var total = results[i].price * answer.stock_quantity;
                         console.log("Your total cost for", answer.stock_quantity, results[i].product_name, "is:", total);
                         // console.log("product_name: " + results[i].product_name + " || department_name: " + results[i].department_name + " || price: " + results[i].price);
@@ -71,12 +84,12 @@ var afterConnection = function () {
                     }
 
                     else {
-                        console.log("Insufficient amount");
+                        console.log("Insufficient Amount");
                     }
                     //if (results[i].stock_quantity - answer.stock_quantity) {
-                        if (answer.stock_quantity <= results[i].stock_quantity) {
-                        var query1=  "UPDATE products SET ? WHERE ?";
-                        connection.query( query1,
+                    if (answer.stock_quantity <= results[i].stock_quantity) {
+                        var query1 = "UPDATE products SET ? WHERE ?";
+                        connection.query(query1,
 
                             [
                                 {
@@ -89,24 +102,22 @@ var afterConnection = function () {
 
                             function (error, results) {
 
+                                console.log("----------------------------------------");
+                                //  console.log("the affected", results.affectedRows);
+                                // console.log(results);
 
-                                console.log("==========================");
-                              //  console.log("the affected", results.affectedRows);
-                               // console.log(results);
-
-                                connection.query("SELECT * FROM products", function(error, results){
-                                    console.log("The updated results:", results);
-
-                                });
+                                //  connection.query("SELECT * FROM products", function (error, results) {
+                                // console.log("The updated results:", results);
+                                showProducts();
 
                             });
-                        }
-                  }
-                
+
+                        //  });
+                    }
+                }
+
                 //console.log("Your products id are: ");
                 // if (error) throw error;
-
-
 
             });
 
@@ -114,7 +125,11 @@ var afterConnection = function () {
     });
 
 }
-afterConnection();
+showProducts();
+
+
+
+
 
 
 
