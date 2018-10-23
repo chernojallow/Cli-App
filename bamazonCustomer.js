38ef6371f543c44458c23b6516d78626525c6810
Zika Chea <jallsadou@gmail.com>
2018-10-22 19:52:24 -0500

    updating

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,19 +1,21 @@
 
-
+// Dependencies
 var mysql = require("mysql");
 var inquirer = require('inquirer');
 var Table = new require('cli-table');
-
+// establish connection to my sql database 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "7082505zika",
+    password: "",
     database: "bamazon"
 });
 
 var showProducts = function () {
 
+    // connect to the database and retrieve the information from  the  product database to display to the user 
+    
     connection.query("SELECT * FROM Products ", function (error, results) {
         if (error) throw error;
         // instantiate table
@@ -24,12 +26,14 @@ var showProducts = function () {
         console.log(" THE TABLE BELOW SHOWS ALL AVAILABLE ITEMS FOR SALE:");
 
         console.log("----------------------------------------------------")
+        //Loop through each item in the mysql database   
         for (var i = 0; i < results.length; i++) {
             table.push([results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
         }
         console.log(table.toString());
         //connection.end();
 
+        ////creates the questions that will be prompted to the user
         inquirer.prompt([
             {
                 name: "item_id",
@@ -62,7 +66,9 @@ var showProducts = function () {
             connection.query(query, { item_id: answer.item_id }, function (error, results) {
 
                 for (var i = 0; i < results.length; i++) {
+                      
 
+				//if the stock amount available is more than or equal to the amount being asked for then the purchase is continued and the user is alerted of what items are being purchased, how much one item is and what the total amount is
                     if (answer.stock_quantity <= results[i].stock_quantity) {
                         console.log("----------------------------------------");
                         var total = results[i].price * answer.stock_quantity;
@@ -72,7 +78,7 @@ var showProducts = function () {
                     else {
                         console.log("Insufficient Amount");
                     }
-                    //if (results[i].stock_quantity - answer.stock_quantity) {
+                  // connect to mysql database Department and updates the total sale 
                     if (answer.stock_quantity <= results[i].stock_quantity) {
                         var query1 = "UPDATE products SET ? WHERE ?";
                         connection.query(query1,
